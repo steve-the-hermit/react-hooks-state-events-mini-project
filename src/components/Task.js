0cd5286f1a857b2a4057c029ecfb302f984e90cc
steve-the-hermit <stevemacharia309@gmail.com>
2023-08-06 23:37:20 +0300

    completed.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-function Task() {
+function Task({ task, onDelete }) {
+  const { text, category, id } = task;
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={() => onDelete(id)}>
+        X
+      </button>
     </div>
   );
 }
